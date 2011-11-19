43f14877b49d5a666c5a46b7fa3ed3e736b576e6
Jon Phenow <j.phenow@gmail.com>
2011-11-18 19:27:41 -0600

    Prepping to fix the ruby issue with reading list files

--- ruby/MergePrefixes.rb
+++ ruby/MergePrefixes.rb
@@ -13,15 +13,15 @@ def fileList(dir, array)
 end
 
 # Source strings
-sources = ['Bash', 'cplusplus', 'java',  'javascript', 'php', 'python', 'ruby']
-#sources = Array.new
+#sources = ['Bash', 'cplusplus', 'java',  'javascript', 'php', 'python', 'ruby']
+sources = Array.new
 
 # Prefix strings
-prefixes = ['ab', 'ba', 'bu', 'Jav', 'ph', 'ru', 'ze']
-#prefixes = Array.new
+#prefixes = ['ab', 'ba', 'bu', 'Jav', 'ph', 'ru', 'ze']
+prefixes = Array.new
 
-#fileList("../lists/sources.list", sources)
-#fileList("../lists/prefixes.list", prefixes)
+fileList("../lists/sources.list", sources)
+fileList("../lists/prefixes.list", prefixes)
 
 # Our ending list
 final = Array.new
